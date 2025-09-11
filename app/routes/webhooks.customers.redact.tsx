eb4c34f97191ebd5b011ba9be5210dad8ce445d6
CØDE <tilanoroser@gmail.com>
2025-09-11 11:38:22 +0200

    chore: resolve eslint issues

--- app/routes/webhooks.customers.redact.tsx
+++ app/routes/webhooks.customers.redact.tsx
@@ -2,7 +2,7 @@ import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { topic, shop, session, admin, payload } = await authenticate.webhook(request);
+  const { shop, admin, payload } = await authenticate.webhook(request);
 
   if (!admin) {
     // The admin context isn't returned if the webhook fired after a shop was uninstalled.
